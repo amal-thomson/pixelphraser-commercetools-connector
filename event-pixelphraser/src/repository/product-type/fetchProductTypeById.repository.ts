d38874bb6dcd437b00ae6a25d1c2e452ac53da90
N AMAL THOMSON <amal.thomson@ayatacommerce.com>
2025-03-25 12:57:06 +0530

    initialize project structure with configuration files, interfaces, and utility functions

---
+++ event-pixelphraser/src/repository/product-type/fetchProductTypeById.repository.ts
@@ -0,0 +1,28 @@
+import { createApiRoot } from '../../client/create.client';
+import { logger } from '../../utils/logger.utils';
+
+export async function fetchProductType(productType: string): Promise<string | null> {
+    try {
+        const apiRoot = createApiRoot();
+
+        logger.info('⌛Fetching product type key for product type ID: ' + productType);
+
+        const productTypeResponse = await apiRoot
+            .productTypes()
+            .withId({ ID: productType })
+            .get()
+            .execute();
+
+        const productTypeKey = productTypeResponse.body.key ?? null;
+
+        logger.info(`✅Product type fetched successfully: ${productTypeKey}`);
+
+        return productTypeKey;
+        
+    } catch (error: any) {
+        logger.error(`🚫Failed to fetch product type for ID: ${productType}`, {
+            message: error.message,
+        });
+        return null;
+    }
+}
