c6b80ea19daa47c53d35fe745afcbca605a8ecb7
N AMAL THOMSON <amal.thomson@ayatacommerce.com>
2025-04-03 17:40:23 +0530

    refactor: remove SelectedLanguagesProvider and simplify rendering in index.tsx

--- mc-pixelphraser/src/index.tsx
+++ mc-pixelphraser/src/index.tsx
@@ -1,18 +1,6 @@
 /// <reference types="@commercetools-frontend/application-config/client" />
 
-// import ReactDOM from 'react-dom';
-// import EntryPoint from './components/entryPoint';
-
-// ReactDOM.render(<EntryPoint />, document.getElementById('app'));
-
-
 import ReactDOM from 'react-dom';
 import EntryPoint from './components/entryPoint';
-import { SelectedLanguagesProvider } from '../src/providers/SelectedLanguagesProvider';
 
-ReactDOM.render(
-  <SelectedLanguagesProvider>
-    <EntryPoint />
-  </SelectedLanguagesProvider>,
-  document.getElementById('app')
-);
+ReactDOM.render(<EntryPoint />, document.getElementById('app'));
